98b6fcf92967ed9a63274ee708aaf4004217af71
adachis <adachis>
2018-09-25 12:31:11 -0500

    Displays all assets

--- DatabaseApp/MainWindow.xaml.cs
+++ DatabaseApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,29 @@ namespace DatabaseApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
         public MainWindow()
         {
             InitializeComponent();
+
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Employees1.accdb");
+        }
+
+        private void SeeAssets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while(read.Read())
+            {
+                data += read[0].ToString() + "\t" + read[1].ToString() + "\t" + read[2].ToString() + "\n";
+            }
+
+            myText1.Text = data;
+
+            cn.Close();
         }
     }
 }
